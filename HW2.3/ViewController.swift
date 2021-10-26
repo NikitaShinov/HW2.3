3bfb76ebcb719f8374867faf9c86a8386b7f4a8b
Nikita Shinov <shinov_96@mail.ru>
2021-10-26 22:04:13 +0300

    Set up constraints for LogOutVC

--- HW2.3/ViewController.swift
+++ HW2.3/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var loginTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func logInPressed(_ sender: UIButton) {
+    }
+    @IBAction func forgotPasswordButtonPressed() {
+    }
+    @IBAction func forgotLoginButtonPressed() {
+    }
 }
 
